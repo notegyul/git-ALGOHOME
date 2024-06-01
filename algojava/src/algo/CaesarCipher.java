3229f08443e3709581ddaddab6eeecf5642e4d69
Mun <sumny1010@gmail.com>
2024-06-01 10:57:06 +0900

    first commit

---
+++ algojava/src/algo/CaesarCipher.java
@@ -0,0 +1,11 @@
+package algo;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = upper.toLowerCase();
+
+        System.out.println(upper);
+        System.out.println(lower);
+    }
+}
---
+++ algojava/src/algo/Test.java
@@ -0,0 +1,7 @@
+package algo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("아하 이제 되네");
+    }
+}
