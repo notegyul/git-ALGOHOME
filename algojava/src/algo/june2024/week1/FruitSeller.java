715db609e01a70ddfa24b65f1bf2133d63d27dc3
Mun <sumny1010@gmail.com>
2024-06-04 16:24:55 +0900

    240601 알고리즘 풀이: [프로그래머스] 과일 장수

---
+++ algojava/src/algo/june2024/week1/FruitSeller.java
@@ -0,0 +1,32 @@
+package algo.june2024.week1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FruitSeller {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int k, int m, int[] score) {
+
+        int answer = 0;
+        // Integer[] 박싱 후 내림차순 정렬
+        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Collections.reverseOrder());
+
+        for (int i=0; i<tmp.length / m; i++) {
+            for (int j=i*m; j<i*m+m; j++) {
+                if (j==i*m+m-1) {
+                    int p = tmp[j];
+                    answer += p * m;
+                }
+            }
+        }
+
+        // 문제를 제대로 안 읽어서 사과 한 상자의 가격이 p*m인 걸 간과하고 반복 돌 때마다 answer에 다 더해버려서 오답이었음
+        return answer;
+
+    }
+}
