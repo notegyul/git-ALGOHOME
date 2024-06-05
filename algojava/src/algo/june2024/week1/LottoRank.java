da1ce7ba5fa4077836cf1541e7e2cb9132460fbd
Mun <sumny1010@gmail.com>
2024-06-06 08:52:41 +0900

    240606 알고리즘 풀이: [프로그래머스] 로또의 최고 순위와 최저 순위

---
+++ algojava/src/algo/june2024/week1/LottoRank.java
@@ -0,0 +1,13 @@
+package algo.june2024.week1;
+
+public class LottoRank {
+    public static void main(String[] args) {
+
+    }
+
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        return answer;
+    }
+}
