efa54cf2114732bb42ed076d80ebbc15dae5a2a5
Mun <sumny1010@gmail.com>
2024-06-08 13:29:50 +0900

    240608 알고리즘 풀이: [프로그래머스] 성격 유형 검사하기

---
+++ algojava/src/algo/june2024/week1/PersonalityTypeTest.java
@@ -0,0 +1,16 @@
+package algo.june2024.week1;
+
+// 240608 - 성격 유형 검사하기
+public class PersonalityTypeTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Solution {
+        public String solution(String[] survey, int[] choices) {
+            String answer = "";
+            return answer;
+        }
+    }
+}
