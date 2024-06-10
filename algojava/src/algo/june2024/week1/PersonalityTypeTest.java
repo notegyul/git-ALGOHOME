a11a6acb832624310afc0c5a09ec149b2a6e7bc8
Mun <sumny1010@gmail.com>
2024-06-10 18:05:17 +0900

    240610 알고리즘 풀이: [프로그래머스] 성격 유형 검사하기 푸는 중 . . .

--- algojava/src/algo/june2024/week1/PersonalityTypeTest.java
+++ algojava/src/algo/june2024/week1/PersonalityTypeTest.java
@@ -14,19 +14,46 @@ public class PersonalityTypeTest {
     public class Solution {
         public String solution(String[] survey, int[] choices) {
 
-            Map<String, Integer> typeMap = new HashMap<>();
             String[] types = {"R", "T", "C", "F", "J", "M", "A", "N"};
-            for (int i=0; i<types.length; i++) {
-                typeMap.put(types[i], (i/2)+1);
+
+            HashMap<String, Integer> innerMap = new HashMap<>();
+            for (int k=0; k<choices.length; k++) {
+                if (choices[k] == 4) {
+                    continue;
+                }
+
+                String character = "";
+                char[] tmpChar = survey[k].toCharArray();
+                // 앞 캐릭터 (1~3) / 뒤 캐릭터 (5~7)
+                if (choices[k] < 4) {
+                    character = String.valueOf(tmpChar[0]);
+                    int point = 4 - choices[k];
+                    innerMap.put(character, innerMap.getOrDefault(character, 0) + point);
+
+                } else {
+                    character = String.valueOf(tmpChar[1]);
+                    int point = choices[k] - 4;
+                    innerMap.put(character, innerMap.getOrDefault(character, 0) + point);
+                }
             }
 
-            
+
+            HashMap<Integer, HashMap<String, Integer>> hm = new HashMap<>();
+            for (int i=0; i<4; i++) {
+                HashMap<String, Integer> newInnerMap = new HashMap<>(innerMap);
+                hm.put(i+1, newInnerMap);
+            }
 
 
+            StringBuffer answer = new StringBuffer();
+            for (Integer typeNum : hm.keySet()) {
 
+                for (String typeStr : hm.get(typeNum).keySet()) {
 
-            String answer = "";
-            return answer;
+                }
+            }
+
+            return answer.toString();
         }
     }
 }
