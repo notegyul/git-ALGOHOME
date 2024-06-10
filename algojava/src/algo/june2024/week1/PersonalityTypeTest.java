8fa57ec4a1f6492666dd179aa0902fdfa058db69
notegyul <remarkmin1095@gmail.com>
2024-06-10 14:21:56 +0900

    240610 알고리즘 풀이: [프로그래머스] 성격 유형 검사하기

--- algojava/src/algo/june2024/week1/PersonalityTypeTest.java
+++ algojava/src/algo/june2024/week1/PersonalityTypeTest.java
@@ -1,14 +1,30 @@
 package algo.june2024.week1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 240608 - 성격 유형 검사하기
 public class PersonalityTypeTest {
 
     public static void main(String[] args) {
 
+
     }
 
     public class Solution {
         public String solution(String[] survey, int[] choices) {
+
+            Map<String, Integer> typeMap = new HashMap<>();
+            String[] types = {"R", "T", "C", "F", "J", "M", "A", "N"};
+            for (int i=0; i<types.length; i++) {
+                typeMap.put(types[i], (i/2)+1);
+            }
+
+            
+
+
+
+
             String answer = "";
             return answer;
         }
---
+++ algojava/src/algo/june2024/week2/Test.java
@@ -0,0 +1,7 @@
+package algo.june2024.week2;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("아하 이제 되네");
+    }
+}
