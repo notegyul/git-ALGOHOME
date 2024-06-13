4f208cec05126010e0fd1a8926eb0c3e9957c92f
Mun <sumny1010@gmail.com>
2024-06-13 13:43:39 +0900

    240613 알고리즘 풀이: [프로그래머스]

---
+++ algojava/src/algo/june2024/week2/CraneDollPicker.java
@@ -0,0 +1,14 @@
+package algo.june2024.week2;
+
+public class CraneDollPicker {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+
+}
--- algojava/src/algo/june2024/week2/Test.java
+++
@@ -1,7 +0,0 @@
-package algo.june2024.week2;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("아하 이제 되네");
-    }
-}
