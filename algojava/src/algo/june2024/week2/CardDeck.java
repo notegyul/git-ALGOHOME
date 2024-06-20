b6b0978838c1667d94316b76355c73cefa5b7dee
notegyul <remarkmin1095@gmail.com>
2024-06-20 16:53:40 +0900

    240620 알고리즘 풀이: [프로그래머스] 카드 뭉치 푸는 중  . . . indexOutOfRange

--- algojava/src/algo/june2024/week2/CardDeck.java
+++ algojava/src/algo/june2024/week2/CardDeck.java
@@ -1,15 +1,34 @@
 package algo.june2024.week2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CardDeck {
     public static void main(String[] args) {
 
     }
 
-    // 260615 - 카드 뭉치
+    // 260620 - 카드 뭉치
     class Solution {
         public String solution(String[] cards1, String[] cards2, String[] goal) {
-            String answer = "";
-            return answer;
+
+            List<String> list1 = new ArrayList<>(Arrays.asList(cards1));
+            List<String> list2 = new ArrayList<>(Arrays.asList(cards2));
+
+            for (String word : goal) {
+                if (!list1.isEmpty() || list1.get(0).equals(word)) {
+                    list1.remove(0);
+                } else {
+                    if (!list2.isEmpty() || list2.get(0).equals(word)) {
+                        list2.remove(0);
+                    } else {
+                        return "No";
+                    }
+                }
+
+            }
+            return "Yes";
         }
     }
 }
