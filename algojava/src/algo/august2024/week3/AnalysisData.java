aea844f225badcbc4d28e4ff11e7ef312af97c58
Mun <sumny1010@gmail.com>
2024-08-13 21:56:02 +0900

    240813 알고리즘 풀이: 데이터 분석 클래스 추가(풀이 전)

---
+++ algojava/src/algo/august2024/week3/AnalysisData.java
@@ -0,0 +1,9 @@
+package algo.august2024.week3;
+
+public class AnalysisData {
+
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        int[][] answer = {};
+        return answer;
+    }
+}
