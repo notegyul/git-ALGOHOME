750e3f02e8d51d2036882115f630b64dea5d33ba
Mun <sumny1010@gmail.com>
2024-08-21 19:42:09 +0900

    240821 알고리즘 풀이: 삼총사 (DFS로 다시 풀어보기)

--- algojava/src/algo/august2024/week4/ThreeIdiots.java
+++ algojava/src/algo/august2024/week4/ThreeIdiots.java
@@ -8,14 +8,19 @@ public class ThreeIdiots {
         int result = 0;
         for (int i=0; i<size-2; i++) {
             for (int j=i+1; j<size-1; j++) {
-                int sum = number[i] + number[j] + number[j+1];
-                if (sum == 0)
-                    result++;
+                for (int k=j+1; k<size; k++) {
+                    int sum = 0;
+                    sum += (number[i] + number[j] + number[k]);
+                    if (sum == 0)
+                        result++;
+                }
             }
         }
 
         return result;
     }
+
+    // 1점짜리 문제라고? 진짜 소름돋는다;;
 
 
 }
