1ea0648f57a5e3d61e045c4a89e4ceea9b62344a
Mun <sumny1010@gmail.com>
2024-08-27 21:44:53 +0900

    240827 알고리즘 풀이: 소수 찾기, 덧칠하기 둘 다 푸는 중^^

---
+++ algojava/src/algo/august2024/week5/FindPrimeNumber.java
@@ -0,0 +1,11 @@
+package algo.august2024.week5;
+
+// 240827 - 소수 찾기(푸는 중^^): 에라토스테네스의 체 그게 먼데~~~~~~~ 나 너무 괴로워
+public class FindPrimeNumber {
+    public int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
---
+++ algojava/src/algo/august2024/week5/OverPainting.java
@@ -0,0 +1,9 @@
+package algo.august2024.week5;
+
+// 240827 - 덧칠하기 (푸는 중^^)
+public class OverPainting {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+}
