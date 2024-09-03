aff64c31c09846e4ca5204217ccbf42bd25fe493
Mun <sumny1010@gmail.com>
2024-09-03 21:19:27 +0900

    240903 알고리즘 풀이: 이진 변환 반복하기, 숫자의 표현

--- algojava/src/algo/september2024/week1/BinaryConversionRepeatedly.java
+++ algojava/src/algo/september2024/week1/BinaryConversionRepeatedly.java
@@ -1,10 +1,23 @@
 package algo.september2024.week1;
 
+import java.util.Stack;
+
 // 240903 - 이진 변환 반복하기
 public class BinaryConversionRepeatedly {
     public int[] solution(String s) {
 
+        int[] result = new int[2];
+        while (s.length() > 1) {
+            // 제거된 0의 개수는 원본에서 "1"만 있는 개수를 뺀 값
+            result[1] += s.length() - s.replace("0", "").length();
+            int c = s.replace("0", "").length();
+            s = Integer.toBinaryString(c);
+            result[0]++;
+        }
 
-        return null;
+        return result;
     }
+
+    // Integer.toBinaryString(int num) 이걸 몰랐네~~~
+    // 이진 변환 직접 구현하다가 머리 터질 뻔~_~
 }
---
+++ algojava/src/algo/september2024/week1/NumberExpression.java
@@ -0,0 +1,23 @@
+package algo.september2024.week1;
+
+// 240903 - 숫자의 표현
+public class NumberExpression {
+    public int solution(int n) {
+        int cnt = 0;
+
+        for (int start=1; start<=n; start++) {
+            int sum = 0;
+            for (int k=start; k<=n; k++) {
+                sum += k;
+                if (sum == n) {
+                    cnt++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
