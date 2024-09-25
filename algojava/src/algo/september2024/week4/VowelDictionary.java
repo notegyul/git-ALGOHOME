d7de0249c26722b463e7f5c961f935e13840ccf4
notegyul <remarkmin1095@gmail.com>
2024-09-25 13:47:32 +0900

    240925 알고리즘 풀이: 모음사전

--- algojava/src/algo/september2024/week4/VowelDictionary.java
+++ algojava/src/algo/september2024/week4/VowelDictionary.java
@@ -1,9 +1,37 @@
 package algo.september2024.week4;
 
-// 240924 - 모음 사전
+import java.util.Set;
+import java.util.TreeSet;
+
+// 240925 - 모음 사전
 public class VowelDictionary {
+    Set<String> set;
+    String[] vowels = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
-        int answer = 0;
-        return answer;
+        set = new TreeSet<>();
+        for (int i=0; i<vowels.length; i++) {
+            vowel(vowels[i], 1);
+        }
+
+        int cnt = 0;
+        for (String s : set) {
+            cnt++;
+            if (s.equals(word))
+                return cnt;
+        }
+
+        return 0;
+    }
+
+    private void vowel(String base, int depth) {
+
+        set.add(base);
+        if (depth == 5)
+            return;
+
+        for (int i=0; i<5; i++) {
+            vowel(base + vowels[i], depth+1);
+        }
+
     }
 }
